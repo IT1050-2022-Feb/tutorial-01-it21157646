78d72520a1b4527ee6f4f070bab6fd3e6a2d6f83
it21157646 <99669675+it21157646@users.noreply.github.com>
2022-05-30 18:20:39 +0000

    tute 2 done

--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,20 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
+
+  int distance,charge;
+
+  printf("enter the distance you travelled  :");
+  scanf("%d",&distance);
+
+  if(distance <= 30){
+    charge = distance * 50;
+    printf("charge for you is  : %d",charge);
+  }
+  else {
+    charge = 1500 + (distance-30);
+    printf("charge for you is  : %d",charge);
+  }
   
   return 0;
 }
